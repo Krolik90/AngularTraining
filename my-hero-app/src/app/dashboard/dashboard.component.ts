63752a96ed0f519e9a4711184a7ac72d225fea6a
mateuszdobrakowski <mateusz.dobrakowski@avanade.com>
2019-03-14 16:41:10 +0100

    routes

--- my-hero-app/src/app/dashboard/dashboard.component.ts
+++ my-hero-app/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
@@ -19,7 +18,7 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => {
-      this.heroes = heroes;
+      this.heroes = heroes.slice(1, 5);
     });
   }
 
